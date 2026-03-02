acd0fd3e42ad42469c3774faff44124b848dc272
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2026-03-02 14:58:07 +0200

    Add new file demonstrating the use of `frozen_string_literal` with examples and explanations of its behavior

---
+++ 15__object_references_and_copies/06__frozen_string_literal.rb
@@ -0,0 +1,18 @@
+# Notes:
+#   * `frozen_string_literal: true` - magic comment that prevents string literals (only literals!) from being modified
+#   * Improves performance by avoiding unnecessary string allocations and mutations
+#   * Should be placed at the top of the file, before any other code
+
+puts "Sviat".object_id #=> 6065
+puts "Sviat".object_id #=> 6345
+
+puts "Sviat" << " is awesome" #=> "Sviat is awesome"
+
+# frozen_string_literal: true
+
+"Sviat".object_id #=> 16
+"Sviat".object_id #=> 16 (same object id because it's frozen)
+
+"Sviat" << " is awesome"              #=> can't modify frozen String: "Sviat" (FrozenError)
+String.new("Sviat") << " is awesome"  #=> "Sviat is awesome" (`String.new` is not a literal)
+"Sviat" << " is awesome"              #=> "Sviat is awesome" (preferred way because it's more efficient)
